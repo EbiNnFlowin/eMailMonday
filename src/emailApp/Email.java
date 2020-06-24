4193f2f4a657e4063dc574a847671431886c5bc6
ebintobin <55466612+ebintobin@users.noreply.github.com>
2020-06-24 16:51:27 -0400

    part 2

--- src/emailApp/Email.java
+++ src/emailApp/Email.java
@@ -6,10 +6,12 @@ public class Email {
     private String firstName;
    private String lastName;
    private String password;
+   private String email;
     private String department;
     private int defaultPasswordLength = 7;
     private int mailboxCapacity;
    private String alternateEmail;
+   private String companySuffix = "computermentors.org";
 
 
 
@@ -22,6 +24,8 @@ public class Email {
         this.password = randomPassword(defaultPasswordLength);
         System.out.println("Department: "+ this.department);
         System.out.println("Your password is: " + this.password);
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() +"@" + department + "."+ companySuffix;
+        System.out.println("Your email is ");
     }
     private  String setDepartment(){
         System.out.print("Enter the department:\n1 for Sales\n2 for Instructors\n3 for Technology\n4 for Office\n0 for none\nEnter department code: ");
